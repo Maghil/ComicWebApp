d26abcc4e4837291ffab9ee9a6cdcc645952c1bf
Maghil Vannan M <mmaghil2k@gmail.com>
2022-10-24 17:26:20 +0530

    initial

---
+++ src/main/java/com/example/comicWebapp/ComicWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.comicWebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/comicWebapp/ComicWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comicWebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
